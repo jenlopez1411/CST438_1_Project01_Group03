438c41be498c2a3851b7feb95ae03e7445b7be70
jenlopez1411 <jennifersophia1411@gmail.com>
2021-09-07 08:52:26 -0700

    This is commit contains the login and CreateAccount page with the Database Needs to be checked by team-members to see if it works properly. (Jennifer)

---
+++ app/src/main/java/com/nayelidj/cst438_1_project01_group03/CreateAccount.java
@@ -0,0 +1,88 @@
+package com.nayelidj.cst438_1_project01_group03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import java.util.List;
+
+public class CreateAccount extends AppCompatActivity {
+    private EditText mUsernameField;
+    private EditText mPasswordField;
+    private Button mButton;
+    private String mUsername;
+    private String mPassword;
+    private CreateAcctDao mCreateDao;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createaccount);
+
+        mUsernameField = findViewById(R.id.usernameEditText);
+        mPasswordField = findViewById(R.id.passwordEditText);
+        mButton = findViewById(R.id.submitBtn);
+        mUsername = mUsernameField.getText().toString();
+        mPassword = mPasswordField.getText().toString();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkUserInDB()){
+                    Toast.makeText(getApplicationContext(), "Already " + mUsername + " exists", Toast.LENGTH_LONG).show();
+                }else{
+                    if(checkValidUsername()){
+                        if(checkValidPassword()){
+                            CreateAcct acct = new CreateAcct(mUsername, mPassword);
+                            mCreateDao.insert(acct);
+                            Toast.makeText(getApplicationContext(), "Account Created Successfully!", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(CreateAccount.this, LogIn.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Password Error: Not Valid Password", Toast.LENGTH_LONG).show();
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Username Error: Not Valid Username", Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            }
+        });
+
+    }
+
+    private boolean checkUserInDB(){
+        List<CreateAcct> acct = mCreateDao.getAllUsers();
+        for(CreateAcct acc: acct){
+            if(mUsername.trim().equals(acc.getUserName().trim())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkValidUsername(){
+        if(mUsername.trim().length() >= 4 && mUsername.matches("(?=.*[a-zA-Z])(?=.*[a-zA-Z])(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$")){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkValidPassword(){
+        if(mPassword.trim().length() >= 4 && mPassword.matches("(?=.*[a-zA-Z])(?=.*[a-zA-Z])(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$")){
+            return true;
+        }
+        return false;
+    }
+    public void getDatabase(){
+        mCreateDao = Room.databaseBuilder(this, CreateAcctDatabase.class, CreateAcctDatabase.DB_NAME)
+                .allowMainThreadQueries()
+                .build()
+                .getCreateAcctDao();
+    }
+}
