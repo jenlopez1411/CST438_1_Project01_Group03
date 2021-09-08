20e0eb863b90feab475079b1f174ceb348016728
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-09-07 19:39:25 -0700

    Created the FavoriteDb, and Dao files. Deleted three user files.

--- app/src/main/java/com/nayelidj/cst438_1_project01_group03/FavoriteDB.java
+++ app/src/main/java/com/nayelidj/cst438_1_project01_group03/FavoriteDB.java
@@ -6,30 +6,27 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class}, version=1, exportSchema = false)
-public abstract class UserDB extends RoomDatabase {
-    public abstract UserDao UserD();
-    private static UserDB sInstance;
-    public static final String User = "user";
+@Database(entities = {Favorite.class}, version=1, exportSchema = false)
+public abstract class FavoriteDB extends RoomDatabase {
+    public abstract FavoriteDao FavD();
+    private static FavoriteDB sInstance;
+    public static final String Favorite = "fav";
 
-    public static synchronized UserDB getInstance(Context context){
+    public static synchronized FavoriteDB getInstance(Context context){
         if (sInstance == null){
             sInstance = Room.databaseBuilder(context.getApplicationContext(),
-                    UserDB.class, "user.db")
+                    FavoriteDB.class, "favorite.db")
                     .allowMainThreadQueries()
                     .build();
         }
         return sInstance;
     }
     public void seed() {
-        if (UserD().countUser() == 0){
+        if (FavD().countFavorite() == 0){
             runInTransaction(new Runnable() {
                 @Override
                 public void run() {
-                    //user added
-                    User student = new User("student", "password");
-
-                    UserD().addUser(student);
+                    //favorite added
                 }
             });
         }
