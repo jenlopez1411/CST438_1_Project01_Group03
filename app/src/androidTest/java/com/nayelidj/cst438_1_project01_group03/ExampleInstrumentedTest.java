0150d0c1c574154e7df0ca85ca3e3ddb3e85e09c
jenlopez1411 <jennifersophia1411@gmail.com>
2021-09-17 13:57:45 -0700

    There is User Database Test within the ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/nayelidj/cst438_1_project01_group03/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nayelidj/cst438_1_project01_group03/ExampleInstrumentedTest.java
@@ -1,14 +1,17 @@
 package com.nayelidj.cst438_1_project01_group03;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.room.Room;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +19,30 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@SmallTest
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        UserDao userDao = (UserDao) Room.databaseBuilder(appContext, UserDatabase.class, UserDatabase.DB_NAME)
+                .allowMainThreadQueries()
+                .build()
+                .getUserDao();
+
+        User user = new User("default", "default");
+        userDao.insert(user);
+        User user2 = userDao.getUser(user.getUserName());
+        assertEquals(user, user);
+
+        user2.setPassword("newDefault");
+        userDao.update(user2);
+
+        User user3 = userDao.getUser(user.getUserName());
+
+        assertNotEquals(user, user3);
+        assertEquals(user2, user3);
+
         assertEquals("com.nayelidj.cst438_1_project01_group03", appContext.getPackageName());
     }
 }
