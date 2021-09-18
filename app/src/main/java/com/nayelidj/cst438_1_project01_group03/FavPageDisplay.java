509320e64ecd2eab44602aefdd87ae54a8a0ee53
Nicky <43733664+Hathoom@users.noreply.github.com>
2021-09-17 20:10:00 -0700

    Fixed Display issues on FavPage Display
    Fixed an issue where the Favorite page wouldn't display everything in the Database.

--- app/src/main/java/com/nayelidj/cst438_1_project01_group03/FavPageDisplay.java
+++ app/src/main/java/com/nayelidj/cst438_1_project01_group03/FavPageDisplay.java
@@ -1,10 +1,12 @@
 package com.nayelidj.cst438_1_project01_group03;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +14,11 @@ import java.util.List;
 
 public class FavPageDisplay extends AppCompatActivity {
     private FavoriteDB fdb;
-    public ListView favListView;
     public List<Favorite> favoriteList;
     public ArrayAdapter<Favorite> FavoriteAdapter;
+
+    private Button doneButton;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +26,41 @@ public class FavPageDisplay extends AppCompatActivity {
         setContentView(R.layout.activity_favorite_job_posts);
         fdb = FavoriteDB.getInstance(this);
         favoriteList = fdb.FavD().getAllFavorite();
-        favListView = findViewById(R.id.list_favorites);
+        textViewResult = findViewById(R.id.text_view_result);
 
         updateUI();
 
+        int i = 1;
+        for (Favorite favorite : favoriteList)
+        {
+            textViewResult.append(String.valueOf(i));
+            textViewResult.append("\n\n");
+            textViewResult.append(favorite.toString());
+            i++;
+        }
+
+        doneButton = findViewById(R.id.done_button);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returntoMain();
+            }
+        });
     }
     private void updateUI() {
         favoriteList = fdb.FavD().getAllFavorite();
         if (FavoriteAdapter == null) {
             FavoriteAdapter = new ArrayAdapter<>(this, R.layout.activity_favorites, R.id.favoritesPageNew, favoriteList);
-            favListView.setAdapter(FavoriteAdapter);
         } else {
             FavoriteAdapter.clear();
             FavoriteAdapter.addAll(favoriteList);
             FavoriteAdapter.notifyDataSetChanged();
-        } }
+        }
+    }
+
+    private void returntoMain()
+    {
+        Intent intent = new Intent(FavPageDisplay.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
